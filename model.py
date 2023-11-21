1b5af39c0ef2f34768a699505345b8ec500d014a
phpin57 <78471883+phpin57@users.noreply.github.com>
2023-11-21 11:13:03 +0100

    Update model.py

--- model.py
+++ model.py
@@ -8,18 +8,18 @@ nclasses = 250
 
 
 class Net(nn.Module):
-    def __init__(self,num_classes=nclasses):
+    def __init__(self):
         super(Net, self).__init__()
-        resnet = models.resnet18(pretrained=True)
-        
-        # Remove the fully connected layer of ResNet-18
-        self.features = nn.Sequential(*list(resnet.children())[:-1])
-        
-        # Add your custom fully connected layer
-        self.fc = nn.Linear(resnet.fc.in_features, num_classes)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, nclasses)
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
