6ac33c636789390bebdf01e67c84d5b7e5bf45cd
phpin57 <78471883+phpin57@users.noreply.github.com>
2023-11-21 13:25:25 +0100

    Update model.py

--- model.py
+++ model.py
@@ -10,17 +10,31 @@ nclasses = 250
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(64),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        
-        # Adjust the input size for your specific images
-        self.fc = nn.Linear(128 * 16 * 16, nclasses)
+
+        self.fc = nn.Sequential(
+            nn.Linear(512 * 16 * 16, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),  # Add dropout for regularization
+            nn.Linear(1024, nclasses)
+        )
 
     def forward(self, x):
         x = self.features(x)
