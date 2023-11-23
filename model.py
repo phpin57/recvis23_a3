3a6cbee02e648f2edd95b2e6ae9a1aba7170f93e
phpin57 <78471883+phpin57@users.noreply.github.com>
2023-11-23 11:31:45 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,30 +11,13 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(256),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-
-        self.fc = nn.Sequential(
-            nn.Linear(512 * 16 * 16, 1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),  # Add dropout for regularization
-            nn.Linear(1024, nclasses)
-        )
+        resnet = models.resnet18(pretrained=True)
+        
+        # Remove the last fully connected layer of ResNet
+        self.features = nn.Sequential(*list(resnet.children())[:-1])
+        
+        # Add your custom fully connected layer for classification
+        self.fc = nn.Linear(resnet.fc.in_features, nclasses)
 
     def forward(self, x):
         x = self.features(x)
