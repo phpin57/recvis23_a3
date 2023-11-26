1f1a15eb3cf375410139190fa248f535968d79c0
phpin57 <78471883+phpin57@users.noreply.github.com>
2023-11-26 12:59:25 +0100

    Update data.py

--- data.py
+++ data.py
@@ -43,6 +43,10 @@ data_transforms_augmented = transforms.Compose([
     transforms.RandomRotation(15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2),
     transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
 
