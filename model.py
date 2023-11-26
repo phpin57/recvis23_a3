a88a3d1d10ef8eb768136b725925a66328ddd751
phpin57 <78471883+phpin57@users.noreply.github.com>
2023-11-26 11:23:34 +0100

    Update model.py

--- model.py
+++ model.py
@@ -57,13 +57,14 @@ class FineTunedNet2(nn.Module):
         self.avgpool=nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(p=dropout_prob)
         self.fc2 = nn.Linear(fc_hidden_size, num_classes)
+
         
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
-        x = self.relu(x)
+        x = F.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
         return x
