cee7f530bf067908394f6c3c71dc618c9b5ea398
phpin57 <78471883+phpin57@users.noreply.github.com>
2023-11-26 14:06:46 +0100

    Update data.py

--- data.py
+++ data.py
@@ -28,6 +28,7 @@ data_transforms_resnet_augm = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.RandomAffine(20),
     transforms.RandomHorizontalFlip(p=0.5),
+    RandomVerticalFlip(),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
     transforms.RandomRotation(20),
     transforms.ToTensor(),
